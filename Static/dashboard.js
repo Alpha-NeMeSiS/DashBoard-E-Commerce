8a8e85517f4491d074e86fffdad6e60b73aa341c
Eliot De Lemos <eliot.delemos@gmail.com>
2025-10-04 16:23:14 +0200

    ajout graph et reorganisation code

--- Static/dashboard.js
+++ Static/dashboard.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const { segLabels, segCounts, segCAMoyen } = window.DASHBOARD_DATA || {};
+  const { segLabels = [], segCounts = [], segCAMoyen = [] } = window.DASHBOARD_DATA || {};
 
   // Graph 1 : Nb clients par segment
   const ctx1 = document.getElementById('segCounts');
@@ -26,6 +26,39 @@ document.addEventListener('DOMContentLoaded', () => {
       options: { responsive: true, plugins: { legend: { display: false } } }
     });
   }
+
+  // Graph 3 : Top clients
+  const ctx3 = document.getElementById('tableaux_Kpi');
+  if (ctx3) {
+    new Chart(ctx3, {
+      type: 'bar',
+      data: {
+        labels: topClientsLabels,
+        datasets: [{
+          label: 'Montant dépensé (€)',
+          data: topClientsValues,
+          borderWidth: 1
+        }]
+      },
+      options: {
+        responsive: true,
+        indexAxis: 'y',
+        plugins: { legend: { display: false } },
+        scales: {
+          x: {
+            beginAtZero: true,
+            // ticks: { color: '#ffffff' },
+            title: { display: true, text: 'Montant (€)' }
+          },
+          y: {
+            // ticks: { color: '#ffffff' },
+            title: { display: true, text: 'Email' }
+          }
+        }
+      }
+    });
+  }
+
 });
 function refreshData() {
   const overlay = document.getElementById("loadingOverlay");
